791fe06a7f4be7b7fc6c5c55ff96483c203d08cd
SlyAdrian <blop.avorad@outlook.fr>
2021-09-20 13:52:34 +0200

    Addition of node_utils file

--- node.py
+++ node.py
@@ -1,5 +1,5 @@
 class Node:
-    def __init__(self, f_value = 0, path_cost = 0 , open = True , location = [0,0]):
+    def __init__(self, location = [0,0], f_value = 0, path_cost = 0 , open = True):
         self.f_value = f_value
         self.path_cost = path_cost
         self.open = open
@@ -17,11 +17,22 @@ class Node:
     def set_location(self, location):
         self.location = location
 
+    def get_f_value(self):
+        return self.f_value
+
     def get_attributes(self):
         print(f'f value : {self.f_value}\t path cost : {self.path_cost}\t open : {self.open}\t location : {self.location}')
+    
+    def __str__(self):
+        print(f'f value : {self.f_value}\t path cost : {self.path_cost}\t open : {self.open}\t location : {self.location}')
+        
 
-n1 = Node(15, 10, True, [0,1])
+
+# Testing code
+
+""" n1 = Node(15, 10, True, [0,1])
 print(n1.get_attributes())
+v1 = n1.get_f_value()
+print(v1)
 n1.set_location(location=[1,2])
-print(n1.get_attributes())
-
+print(n1.get_attributes()) """
