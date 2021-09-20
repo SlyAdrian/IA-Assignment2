75d4a778568289f5e2e87de467d9eb090e84bf1a
SlyAdrian <blop.avorad@outlook.fr>
2021-09-20 19:52:05 +0200

    Beginning of the main

--- A Star.py
+++ A Star.py
@@ -1,13 +1,29 @@
 import numpy as np
+import time
+import pandas as pd
+
+from PIL import Image
+from Map import Map_Obj
+
 
 np.set_printoptions(threshold=np.inf, linewidth=300)
-import time
-
-import pandas as pd
-from PIL import Image
 
 def main():
     print("Start of the A*")
+
+    #### TASK 1 ####
+
+    # Initialization of the map 
+    
+    # Initialization of the 2 lists
+    openList = []
+    closedList = []
+    
+    task1 = Map_Obj(task=1)
+
+    # Beginning of the algorithm
+    openList.append(task1.get_start_pos)
+
     pass
 
 
